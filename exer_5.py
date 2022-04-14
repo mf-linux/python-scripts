8f55d511304355c7373732ccf344cd6b5082135a
Marcus Fermino <marcusfermino.linux@gmail.com>
2022-04-14 17:41:49 -0300

    Alteração

---
+++ exer_5.py
@@ -0,0 +1,20 @@
+# exer_5.py
+
+# Descrição
+#   Percorre os caracteres de uma String
+
+# Versão 1.0
+
+# Author: Marcus Fermino
+
+# GitHub: https://www.github.com/mf-linux
+
+nome = 'Marcus Fermino'
+
+print(nome[0])                      # Primeiro caractere
+print(nome[2])                      # Terceiro caractere
+print(nome[4])                      # Quinto caractere
+print(nome[6])                      # Setimo caractere 
+
+print(nome[0:3])                    # Intervalo de caracteres
+print(nome[3:7])                    # Intervalo de caracteres
